5d3a30ca1cd73217d9c0699ea7dede6004ea5acb
glazed-heart <2931798419@qq.com>
2022-10-22 17:16:13 +0800

    前端架构

---
+++ src/layout/content/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="">内容</div>
+</template>
+
+<script setup lang="ts">
+import { } from 'vue'
+</script>
+
+<style lang="less" scoped>
+
+</style>
---
+++ src/layout/header/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="">头部</div>
+</template>
+
+<script setup lang="ts">
+import { } from 'vue'
+</script>
+
+<style lang="less" scoped>
+
+</style>
